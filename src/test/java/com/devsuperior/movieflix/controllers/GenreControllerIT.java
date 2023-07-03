fdd64db611dc29d5d8cd24123927c9d5dd2c353d
Lucas Galvao <lucas.engcomp@outlook.com>
2023-07-03 18:21:32 -0300

    :sparkles: `feat` Removed public tests

--- src/test/java/com/devsuperior/movieflix/controllers/GenreControllerIT.java
+++ src/test/java/com/devsuperior/movieflix/controllers/GenreControllerIT.java
@@ -1,9 +1,6 @@
 package com.devsuperior.movieflix.controllers;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
+import com.devsuperior.movieflix.tests.TokenUtil;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,78 +11,80 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.devsuperior.movieflix.tests.TokenUtil;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
 @Transactional
-public class GenreControllerIT {
+class GenreControllerIT {
 
-	@Autowired
-	private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
-	@Autowired
-	private TokenUtil tokenUtil;
-	
-	private String visitorUsername;
-	private String visitorPassword;
-	private String memberUsername;
-	private String memberPassword;
-	
-	@BeforeEach
-	void setUp() throws Exception {
-		
-		visitorUsername = "bob@gmail.com";
-		visitorPassword = "123456";
-		memberUsername = "ana@gmail.com";
-		memberPassword = "123456";
-	}
+    @Autowired
+    private TokenUtil tokenUtil;
 
-	@Test
-	public void findAllShouldReturnUnauthorizedWhenNotValidToken() throws Exception {
+    private String visitorUsername;
+    private String visitorPassword;
+    private String memberUsername;
+    private String memberPassword;
 
-		ResultActions result =
-				mockMvc.perform(get("/genres")
-					.contentType(MediaType.APPLICATION_JSON));
+    @BeforeEach
+    void setUp() throws Exception {
 
-		result.andExpect(status().isUnauthorized());
-	}
-	
-	@Test
-	public void findAllShouldReturnAllGenresWhenVisitorAuthenticated() throws Exception {
+        visitorUsername = "bob@gmail.com";
+        visitorPassword = "123456";
+        memberUsername = "ana@gmail.com";
+        memberPassword = "123456";
+    }
 
-		String accessToken = tokenUtil.obtainAccessToken(mockMvc, visitorUsername, visitorPassword);
-		
-		ResultActions result =
-				mockMvc.perform(get("/genres")
-					.header("Authorization", "Bearer " + accessToken)
-					.contentType(MediaType.APPLICATION_JSON));
+    @Test
+    void findAllShouldReturnUnauthorizedWhenNotValidToken() throws Exception {
 
-		result.andExpect(status().isOk());
-		result.andExpect(jsonPath("$[0].id").value(1L));
-		result.andExpect(jsonPath("$[0].name").value("Comédia"));
-		result.andExpect(jsonPath("$[1].id").value(2L));
-		result.andExpect(jsonPath("$[1].name").value("Terror"));
-		result.andExpect(jsonPath("$[2].id").value(3L));
-		result.andExpect(jsonPath("$[2].name").value("Drama"));
-	}
-	
-	@Test
-	public void findAllShouldReturnAllGenresWhenMemberAuthenticated() throws Exception {
+        ResultActions result =
+                mockMvc.perform(get("/genres")
+                        .contentType(MediaType.APPLICATION_JSON));
 
-		String accessToken = tokenUtil.obtainAccessToken(mockMvc, memberUsername, memberPassword);
+        result.andExpect(status().isUnauthorized());
+    }
 
-		ResultActions result =
-				mockMvc.perform(get("/genres")
-					.header("Authorization", "Bearer " + accessToken)
-					.contentType(MediaType.APPLICATION_JSON));
+    @Test
+    void findAllShouldReturnAllGenresWhenVisitorAuthenticated() throws Exception {
 
-		result.andExpect(status().isOk());
-		result.andExpect(jsonPath("$[0].id").value(1L));
-		result.andExpect(jsonPath("$[0].name").value("Comédia"));
-		result.andExpect(jsonPath("$[1].id").value(2L));
-		result.andExpect(jsonPath("$[1].name").value("Terror"));
-		result.andExpect(jsonPath("$[2].id").value(3L));
-		result.andExpect(jsonPath("$[2].name").value("Drama"));		
-	}
+        String accessToken = tokenUtil.obtainAccessToken(mockMvc, visitorUsername, visitorPassword);
+
+        ResultActions result =
+                mockMvc.perform(get("/genres")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$[0].id").value(1L));
+        result.andExpect(jsonPath("$[0].name").value("Comédia"));
+        result.andExpect(jsonPath("$[1].id").value(2L));
+        result.andExpect(jsonPath("$[1].name").value("Terror"));
+        result.andExpect(jsonPath("$[2].id").value(3L));
+        result.andExpect(jsonPath("$[2].name").value("Drama"));
+    }
+
+    @Test
+    void findAllShouldReturnAllGenresWhenMemberAuthenticated() throws Exception {
+
+        String accessToken = tokenUtil.obtainAccessToken(mockMvc, memberUsername, memberPassword);
+
+        ResultActions result =
+                mockMvc.perform(get("/genres")
+                        .header("Authorization", "Bearer " + accessToken)
+                        .contentType(MediaType.APPLICATION_JSON));
+
+        result.andExpect(status().isOk());
+        result.andExpect(jsonPath("$[0].id").value(1L));
+        result.andExpect(jsonPath("$[0].name").value("Comédia"));
+        result.andExpect(jsonPath("$[1].id").value(2L));
+        result.andExpect(jsonPath("$[1].name").value("Terror"));
+        result.andExpect(jsonPath("$[2].id").value(3L));
+        result.andExpect(jsonPath("$[2].name").value("Drama"));
+    }
 }
