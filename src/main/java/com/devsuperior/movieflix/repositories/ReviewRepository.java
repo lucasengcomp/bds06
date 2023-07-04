827fd4389dfd13832f1422dd17d942f96d78c518
Lucas Galvao <lucas.engcomp@outlook.com>
2023-07-03 23:35:13 -0300

    :sparkles: `feat` Repositories

---
+++ src/main/java/com/devsuperior/movieflix/repositories/ReviewRepository.java
@@ -0,0 +1,11 @@
+package com.devsuperior.movieflix.repositories;
+
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+}
